85a6808780412346f4932bc4d1f018c52afc7073
chrismamo1 <christophermcalpine@gmail.com>
2018-05-06 01:10:07 -0600

    show error

--- preprocess.fish
+++ preprocess.fish
@@ -4,8 +4,9 @@ for i in $argv
   set tName (mktemp);
   convert $i $tName.ppm;
   #set generatedFiles (./main -x 'detect edges' -c 2 -d 2 -s 1.1 -k 3 -l 100 $tName.ppm out/$i.ppm);
-  set generatedFiles (./main -c 2 -d 2 -s 1.1 -k 3 -l 100 $tName.ppm out/$i.ppm);
+  set generatedFiles (./main -c 2 -d 2 -s 1.1 -k 3 -l 100 $tName.ppm out/(basename $i).ppm);
   for j in $generatedFiles
+    echo "j = " %j;
     convert $j $j.png
     rm $j
   end
